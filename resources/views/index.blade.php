6b415de18238fc83a3929ffdc141da8ee3ac3be4
Alejandro <alejandro@example.com>
2026-03-15 22:52:42 +0100

    Index, nav, footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,12 @@
-<h1>Bienvenido a mi sitio web</h1>
+@extends('layout.layout')
+
+@section('title')
+Inicio - QueComoHoy
+@endsection
+@section('content')
+    <div class="container">
+        <h1>Bienvenido a QueComoHoy</h1>
+        <p>Descubre recetas deliciosas y saludables para cada día.</p>
+        <a href="{{ route('recipes.index') }}" class="btn btn-primary">Ver Recetas</a>
+    </div>
+@endsection
