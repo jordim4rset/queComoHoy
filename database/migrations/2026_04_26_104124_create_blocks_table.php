4a3aadf7af285b1d7937187ffb8db88dee8045ab
Elias <eliasmahyaoui6@hotmail.com>
2026-04-26 13:30:39 +0200

    BLOCK ACTUALIZADO

---
+++ database/migrations/2026_04_26_104124_create_blocks_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up()
+{
+    Schema::create('blocks', function (Blueprint $table) {
+        $table->integer('id_bloqueador');
+        $table->integer('id_bloqueado');
+    });
+}
+
+public function down()
+{
+    Schema::dropIfExists('blocks');
+}
+};
