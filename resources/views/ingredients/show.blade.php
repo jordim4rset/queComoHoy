373c1d9ba02187c6de42a478f1a31a752c3b0c15
Elias <eliasmahyaoui6@hotmail.com>
2026-05-04 00:56:43 +0200

    Cambios en el tamaño del show de ingredientes

--- resources/views/ingredients/show.blade.php
+++ resources/views/ingredients/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.layout')
-
-@section('title', 'Ver  Ingredientes')
-
+@section('title', 'Ver Ingredientes')
 @section('content')
-    <h1>{{ $ingrediente->name }}</h1>
-    <p>{{ $ingrediente->category }}</p>
-    <img src="{{ asset('storage/' . $ingrediente->icon) }}" alt="Icono {{ $ingrediente->name }}" onerror="this.style.display='none'">
-
-    <a href="{{ route('ingredientes.edit', $ingrediente) }}" class="btn">Editar</a>
-    <a href="{{ route('ingredientes.index') }}" class="btn">Volver</a>
+    <div class="card-content">
+        <img src="{{ asset('storage/' . $ingrediente->icon) }}" alt="Icono {{ $ingrediente->name }}"
+            onerror="this.style.display='none'">
+        <h1>{{ $ingrediente->name }}</h1>
+        <p>{{ $ingrediente->category }}</p>
+        <a href="{{ route('ingredientes.edit', $ingrediente) }}" class="btn">Editar</a>
+        <a href="{{ route('ingredientes.index') }}" class="btn">Volver</a>
+    </div>
 @endsection
